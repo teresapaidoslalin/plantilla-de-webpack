2749a384bc455c7572ca498bb58cb38a03d42596
Teresa <teresa.paidoslalin@gmail.com>
2022-02-17 11:29:07 +0100

    commit inicial

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpack=require('html-webpack-plugin');
+const MiniCssExtract= require("mini-css-extract-plugin");
+
+module.exports={
+    mode: 'development',
+    output:{
+        clean:true
+    },
+    module:{
+        rules:[
+            {
+                test:/\.html$/,
+                loader:'html-loader',
+                options:{
+                    sources:false
+                }
+            },
+            {
+                test:/\.css$/,
+                exclude:/styles.css$/,
+                use:['style-loader','css-loader']
+            },
+            {
+                test:/styles.css$/,
+                use:[MiniCssExtract.loader,'css-loader']
+            },
+            {
+                test:/\.(png|jpe?g|gif|svg)$/,
+                loader:'file-loader'
+            }
+        ]
+    },
+    optimization:{},
+    plugins:[
+        new HtmlWebpack({
+            title:'Mi primer webpack',
+            template:'./src/index.html'
+        }),
+
+        new MiniCssExtract({
+            filename:'[name].css',
+            ignoreOrder:false
+        }),
+        new CopyPlugin({
+            patterns:[
+                {from:'src/assets',to:'assets'}
+            ]
+            
+        })
+    ]
+}
